e3f38b467e3553a1182579cf1b7b605a662f70a4
Meghana Athanti <maggimeghana975@gmail.com>
2026-02-03 10:15:15 +0530

    Added navbar and home page

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav style={{ padding: "15px", backgroundColor: "#1976d2", color: "white" }}>
+      <h3>College Utility App</h3>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ marginTop: "40px", textAlign: "center" }}>
+      <h2>Welcome</h2>
+      <p>This app helps students access college resources.</p>
+    </div>
+  );
+}
+
+export default Home;
